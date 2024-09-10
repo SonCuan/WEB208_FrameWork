c4dd4f19bf0a1451fe4d1fa35310a13bc75c87b3
SonCuan <sonquan2611@gmail.com>
2024-09-10 16:01:37 +0700

    Buoi 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'My_Angular';
-}
+  student: {
+    fullName : string,
+    gender : "Nam" | "Nu" , 
+    age : number,
+    picture : string, 
+    agvScore : number,
+  } = {
+    fullName : "Nguyen Van A", 
+    gender : "Nam",
+    age : 20,
+    picture : "https://picsum.photos/200",
+    agvScore : 10
+  }
+  getProfile (){ 
+    return `trash`
+  }
+ }
